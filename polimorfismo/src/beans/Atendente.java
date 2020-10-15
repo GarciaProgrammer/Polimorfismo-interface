bca941a551b36c43f6f9bd1274ffeeb4c98e23fc
teste <Gaaskdoa@gmail.com>
2020-10-15 18:42:20 -0300

    atendente

---
+++ polimorfismo/src/beans/Atendente.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class Atendente extends Funcionario {
+    private String chaveAcessoTelefone;
+
+    public String getChaveAcessoTelefone() {
+        return chaveAcessoTelefone;
+    }
+
+    public void setChaveAcessoTelefone(String chaveAcessoTelefone) {
+        this.chaveAcessoTelefone = chaveAcessoTelefone;
+    }
+    
+    
+}
